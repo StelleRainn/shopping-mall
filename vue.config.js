348661c5a5cce85268b43b68577cca75b6b7cbf6
StelleRainn R.M. <3298257615@qq.com>
2025-09-09 20:55:21 +0800

    Create Github Pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // 使网站可以使用相对路径来寻找文件，不强制将打包后的文件放在服务器根目录下
-  publicPath: './',
+  // // 使网站可以使用相对路径来寻找文件，不强制将打包后的文件放在服务器根目录下
+  // publicPath: './',
+
+  // 为部署到 GitHub Pages，作如下修改：
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/shopping-mall-vue2-demo/'
+    : '/',
   transpileDependencies: true
 })
